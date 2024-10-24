243023031c227bf59b195e8372a260342eb45762
GMJSilmaro <dzheysilmaro@gmail.com>
2024-10-24 15:15:24 +0800

    update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAN7o4-N5unSUCtU7amZKRA4TRV7WjJ01Y",
-  authDomain: "sas-fsm.firebaseapp.com",
-  projectId: "sas-fsm",
-  storageBucket: "sas-fsm.appspot.com",
-  messagingSenderId: "653364522600",
-  appId: "1:653364522600:web:fc90ef9dedda2181a9d5dd",
-  measurementId: "G-JX36QS06TM"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
